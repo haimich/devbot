2856fdb79a62b4246daaf94cd90f3199e1ee1dc7
Michael Mueller <haimich@gmx.net>
2017-12-09 13:58:22 +0100

    prepared next widget

--- src/components/routes/Misc.vue
+++ src/components/routes/Misc.vue
@@ -27,19 +27,41 @@
               <el-button type="primary" @click="convertTimestamp" plain>Timestamp to Human date</el-button>
             </el-col>
           </el-row>
+        </el-form-item>
 
-          <el-row v-if="timestamp.calculated.value !== null" class="timestamp-results">
-            <div v-if="timestamp.calculated.type === 'microseconds'" class="timestamp-notice">Assuming that this timestamp is in microseconds</div>
-            <div v-if="timestamp.calculated.type === 'milliseconds'" class="timestamp-notice">Assuming that this timestamp is in milliseconds</div>
+        <el-row v-if="timestamp.calculated.value !== null" class="timestamp-results">
+          <div v-if="timestamp.calculated.type === 'microseconds'" class="timestamp-notice">Assuming that this timestamp is in microseconds</div>
+          <div v-if="timestamp.calculated.type === 'milliseconds'" class="timestamp-notice">Assuming that this timestamp is in milliseconds</div>
 
-            <div>
-              <strong>GMT: </strong> {{this.timestamp.calculated.valueGMT}}
-            </div>
-            <div>
-              <strong>Your time zone: </strong> {{this.timestamp.calculated.value}}
-            </div>
+          <div>
+            <strong>GMT: </strong> {{this.timestamp.calculated.valueGMT}}
+          </div>
+          <div>
+            <strong>Your time zone: </strong> {{this.timestamp.calculated.value}}
+          </div>
+        </el-row>
+
+        <el-form-item label="Select time">
+          <el-row :gutter="style.gutter">
+            <el-col :span="8">
+              <el-date-picker
+                v-model="timestamp.selectedDatetime"
+                type="datetime"
+                placeholder="Select date and time">
+              </el-date-picker>
+            </el-col>
+            <el-col :span="4">
+              <el-button type="primary" @click="convertDatetime" plain>Human date to Timestamp</el-button>
+            </el-col>
           </el-row>
         </el-form-item>
+
+        <div style="margin-top: 20px">
+          <el-radio-group v-model="timestamp.selectedTimezone" size="small">
+            <el-radio-button label="GMT"></el-radio-button>
+            <el-radio-button label="CET"></el-radio-button>
+          </el-radio-group>
+        </div>
     </el-form>
   </div>
 </template>
@@ -61,6 +83,8 @@ export default {
       timestamp: {
         current: this.getCurrentTimestamp(),
         userInput: '',
+        selectedDatetime: null,
+        selectedTimezone: "GMT",
         calculated: {
           value: null,
           type: '',
@@ -129,6 +153,23 @@ export default {
       this.timestamp.calculated.value = momentObj.tz("CET").format("dddd, MMMM Do YYYY, HH:mm:ss");
     },
 
+    convertDatetime() {
+      if (this.timestamp.selectedDatetime == null || this.timestamp.selectedDatetime === '') {
+        return;
+      }
+
+      let value = this.timestamp.selectedDatetime;
+      var momentObj = moment(value);
+
+      if (! momentObj.isValid()) {
+        this.notify({ text: "Is not valid: " + value, duration: 2000 });
+        return;
+      }
+
+      this.timestamp.calculated.valueGMT = momentObj.tz("GMT").format("dddd, MMMM Do YYYY, HH:mm:ss");
+      this.timestamp.calculated.value = momentObj.tz("CET").format("dddd, MMMM Do YYYY, HH:mm:ss");
+    },
+
     getCurrentTimestamp() {
       return moment().unix(); // returns seconds
     },
@@ -172,7 +213,7 @@ export default {
 }
 
 .timestamp-results {
-  margin-top: 15px;
+  font-size: 14px;
   line-height: 20px;
 }
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en'
 
 import router from '@/router'
 import App from './App'
@@ -10,7 +11,7 @@ import Solr from '@/components/widgets/Solr'
 
 Vue.config.productionTip = false
 
-Vue.use(ElementUI)
+Vue.use(ElementUI, { locale })
 
 Vue.component('solr-widget', Solr);
 
