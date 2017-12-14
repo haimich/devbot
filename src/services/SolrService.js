9a63737ad5d1df53d669b840e2406983894fff35
Michael Mueller <michael.mueller@echobot.de>
2017-12-14 17:27:50 +0100

    added service

---
+++ src/services/SolrService.js
@@ -0,0 +1,28 @@
+
+export default {
+
+  getActiveSolr() {
+    return fetch("/backend/solr/active?env=production")
+      .then(response => {
+        return response.json();
+      })
+      .then(servers => {
+        for (let server of servers) {
+          server.urlPath = this.getUrlPath(server.url);
+        }
+
+        return servers;
+      })
+      .catch(err => {
+        return err;
+      });
+  },
+
+  getUrlPath(url) {
+    var parser = document.createElement('a');
+    parser.href = url;
+    
+    return parser.hostname;
+  }
+
+}
